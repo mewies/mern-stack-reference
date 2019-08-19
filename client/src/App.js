ed476199326479e72df4ff822a5b6b5faa7b613f
Mohamed Ewies <mewies@eg.ibm.com>
2019-08-19 16:04:20 +0200

    Adding initial client code

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,26 @@
-import React, {
-  Component
-} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing}></Route>
+        <section className='container'>
+          <Switch>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
+        </section>
+        
+      </Fragment>
+    </Router>
+   
+);
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,102 +1,88 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import classnames from 'classnames'
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
 
-class Register extends Component {
+  const { name, email, password, password2 } = formData;
+  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      email: '',
-      password: '',
-      password2: '',
-      errors: {}
-    };
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit(e) {
+  const onSubmit = async e => {
     e.preventDefault();
-
-    const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2
+    if(password !== password2) {
+      console.log('Passwords do not match');
     }
-    axios.post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
+    else {
+      const newUser = {
+        name,
+        email,
+        password,
+        password2
+      }
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+        const body = JSON.stringify(newUser);
+        const res = await axios.post('/api/users/register', body, config);
+        console.log(res.data);
+      }
+      catch(err) {
+        console.log(err.response.data);
+      }
+    }
   }
 
-  render() {
-    const { errors } = this.state;
-    console.log(errors);
-
-    return (
-      <div className="register">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your DevConnector account</p>
-              <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                  <input type="text"
-                    className={classnames('form-control form-control-lg', {
-                      'is-invalid': errors.name
-                    })}
-                    placeholder="Name" name="name"
-                    value={this.state.name}
-                    onChange={this.onChange} />
-                  {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
-                </div>
-                <div className="form-group">
-                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"
-                    value={this.state.email}
-                    onChange={this.onChange}
-                    className={classnames('form-control form-control-lg', {
-                      'is-invalid': errors.email
-                    })}
-                  />
-                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
-                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
-                </div>
-                <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"
-                    value={this.state.password}
-                    onChange={this.onChange}
-                    className={classnames('form-control form-control-lg', {
-                      'is-invalid': errors.password
-                    })}
-                  />
-                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
-                </div>
-                <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2"
-                    value={this.state.password2}
-                    onChange={this.onChange}
-                    className={classnames('form-control form-control-lg', {
-                      'is-invalid': errors.password2
-                    })}
-                  />
-                  {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
-                </div>
-                <input type="submit" className="btn btn-info btn-block mt-4" />
-              </form>
-            </div>
-          </div>
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit= {e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" value={name} required
+          onChange = {e => onChange(e)} />
         </div>
-      </div>
-    )
-  }
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} required
+          onChange = {e => onChange(e)} />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} required
+            onChange = {e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2} required
+            onChange = {e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </Fragment>
+  )
 }
 
-export default Register;
+export default Register
